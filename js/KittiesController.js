58d14f654b9571a5435b0b0efaa69b9d15370bcd
Eunice Park <parkeunicem@gmail.com>
2015-03-27 22:26:14 -0700

    final commit

--- js/KittiesController.js
+++ js/KittiesController.js
@@ -12,32 +12,28 @@
 
 		this.cats =
 		[
-			{
-				catName: 'Karl',
+			{	catName: 'Karl',
 				monthsOld: 5,
 				gender: 'boy',
 				image: "images/carl.png",
 				adopted: true,
 				status: "Adopted"
 			},
-			{
-				catName: 'Jack',
+			{	catName: 'Jack',
 				monthsOld: 4,
 				gender: 'boy',
 				image: "images/jack.png",
 				adopted: false,
 				status: "Ready for Adoption"
 			},
-			{
-				catName: 'Oscar',
+			{	catName: 'Oscar',
 				monthsOld: 2,
 				gender: 'boy',
 				image: "images/oscar.png",
 				adopted: false,
 				status: "Ready for Adoption"
 			},
-			{
-				catName: 'Princess Mew',
+			{	catName: 'Princess Mew',
 				monthsOld: 3,
 				gender: 'girl',
 				image: "images/princessmew.png",
@@ -48,16 +44,9 @@
 		];
 
 	
-
-		function deleteKittie($index) {
-			// if (self.cats[name].catName === name) {
-
-			// for (i=0;i<self.cats.length;i++) {
-			// 	self.itemIndex = self.cats[i].name = name;
-			// }
-						
-			// self.cats.splice(self.itemIndex,1);
-			self.cats.splice($index,1);
+		function deleteKittie(cat) {
+			console.log(self.cats.indexOf(cat));
+			self.cats.splice(self.cats.indexOf(cat),1);
 			// location.reload();
 		}
 	}
